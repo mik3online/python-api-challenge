78b15f14ac09289a7f61e211c26d65c98dec04bf
mikestrati-VXEjhg <mikestrati@gmail.com>
2023-07-14 22:44:13 -0700

    updated api keys

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "ee8c77a6eb454999f24dd6ac17b3e0fd"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "113dabe380974090a5a56a3d529d71b5"
